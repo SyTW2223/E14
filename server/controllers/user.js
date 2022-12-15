3c0433f4e384d550ae0c834df72d8ca89eab5d9a
Eduardo Da Silva <alu0101104911@ull.edu.es>
2022-12-15 17:53:54 +0000

    Eliminar usuario de BBDD + testing

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -10,6 +10,7 @@ async function getMe(req, res) {
     res.status(200).send(response)
   }
 }
+
 async function getUsers(req, res) {
   //const { active } = req.query;
   let response = null;
@@ -17,7 +18,21 @@ async function getUsers(req, res) {
 
   res.status(200).send(response);
 }
+
+async function deleteUser(req, res) {
+  const { id } = req.params;
+
+  User.findByIdAndDelete(id, (error) => {
+    if (error) {
+      res.status(400).send({msg: "Error al eliminar el usuario especificado"});
+    } else {
+      res.status(200).send({msg: "Eliminacion del usuario satisfactoria"});
+    }
+  })
+}
+
 module.exports = {
   getMe,
-  getUsers
+  getUsers,
+  deleteUser,
 };
