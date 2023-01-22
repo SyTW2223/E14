a73734ffb882a5a0b4bed1217e77110ccdd14160
Eduardo Da Silva <alu0101104911@ull.edu.es>
2023-01-22 19:26:52 +0000

    Prittier

--- marvelapp/src/router/AdminRouter.js
+++ marvelapp/src/router/AdminRouter.js
@@ -1,41 +1,37 @@
-import React from 'react';
-import {Routes, Route} from "react-router-dom";
-import {Auth, Users, Blog, SingleUser} from "../pages/admin";
-import {AdminLayout} from "../layouts";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Auth, SingleUser } from "../pages/admin";
+import { AdminLayout } from "../layouts";
 //import { map } from "lodash";
 import { useAuth } from "../hooks";
 
+import { Home } from "../pages/web/Home";
 
-import {Home} from "../pages/web/Home";
+export function AdminRouter() {
+  const { user } = useAuth();
 
-
-
-export  function AdminRouter() {
-    const { user } = useAuth();
-    
-    const loadLayout = (Layout, Page) => {
-        return (
-            <Layout>
-                <Page />
-            </Layout>
-        );
-
-    };
+  const loadLayout = (Layout, Page) => {
+    return (
+      <Layout>
+        <Page />
+      </Layout>
+    );
+  };
   return (
     <Routes>
-     {!user ? (        
-        <Route path="/auth/*" element = {<Auth/>} />
-        ) : (
+      {!user ? (
+        <Route path="/auth/*" element={<Auth />} />
+      ) : (
         <>
-        {["/auth", "/auth/me"].map((path) => (
+          {["/auth", "/auth/me"].map((path) => (
             <Route
-            key={path}
-            path={path}
-            element={loadLayout(AdminLayout, SingleUser)}
+              key={path}
+              path={path}
+              element={loadLayout(AdminLayout, SingleUser)}
             />
-        ))}
+          ))}
         </>
-       )}
+      )}
     </Routes>
   );
 }
