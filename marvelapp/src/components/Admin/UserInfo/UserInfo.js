c433ff1e0223807e46b564e52af1df14b2e1d0b4
Eduardo Da Silva <alu0101104911@ull.edu.es>
2023-01-22 17:04:21 +0000

    Perfil de usuario formateado y retocado home

--- marvelapp/src/components/Admin/UserInfo/UserInfo.js
+++ marvelapp/src/components/Admin/UserInfo/UserInfo.js
@@ -1,10 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { useAuth } from "../../../hooks";
 import { DateTime } from "luxon";
-import {Container} from "semantic-ui-react";
+import { Container } from "semantic-ui-react";
 
-
-import "./UserInfo.scss"
+import "./UserInfo.scss";
 
 export function UserInfo() {
   const { user } = useAuth();
@@ -17,31 +16,58 @@ export function UserInfo() {
   //setComics();
   return (
     <div>
-      <h1>Este es el componente con la info del usuario</h1>
-      <div class="row">
-    <div class="column"></div>
-    <div class="column"></div>
+      <div className="zona_info_usuario">
+        <h1 className="titulos_secciones">Información de tu usuario: {user.nickname}</h1>
+        <Container classname="container_user">
+          <div className="texto_usuario">
+            <h3 className="titulo_info"> Nickname:</h3>
+            <div className="texto_info">{user.nickname}</div>
+            <div class="ui divider"></div>
+            <h3 className="titulo_info"> Nombre:</h3>
+            <div className="texto_info">{user.nombre}</div>
+            <div class="ui divider"></div>
+            <h3 className="titulo_info"> Edad:</h3>
+            <div className="texto_info">{user.edad}</div>
+            <div class="ui divider"></div>
+            <h3 className="titulo_info"> Se unió a la comunidad el día:</h3>
+            <span className="texto_info">
+              {DateTime.fromISO(date.toISOString())
+                .setLocale("es")
+                .toFormat("dd 'de' LLLL 'del' yyyy")}
+            </span>
+          </div>
+          <div className="zona_imagen">
+            <img
+              className="imagen_usuario"
+              src={"https://cdn-icons-png.flaticon.com/512/147/147144.png"}
+              alt={"Avatar del usuario"}
+            />
+          </div>
+        </Container>
+      </div>
+      <div className="zonaGustos">
+        <h2 className="titulos_secciones">Tus gustos</h2>
+        <Container classname="container_gustos">
+          <div className="cosas_fav">
+            <h3 className="titulo_cosas_fav">Comic favoritos</h3>
+            <div><div class="ui divider"></div>
+              {user.comicsFav.map((comic) => {
+                return <div className="info_cosas_fav">{comic}</div>;
+              })}
+            </div>
+            <div>{user.comicsFav.map}</div>
+          </div>
+          <div className="cosas_fav">
+            <h3 className="titulo_cosas_fav">Personajes favoritos</h3>
+            <div>
+            <div class="ui divider"></div>
+              {user.personajesFav.map((heroe) => {
+                return <div className="info_cosas_fav">{heroe}</div>;
+              })}
+            </div>
+          </div>
+        </Container>
+      </div>
     </div>
-      <div>{user.nickname}</div>
-      <div>{user.nombre}</div>
-      <div>{user.edad}</div>
-      <span>
-        Se unió el 
-            {DateTime.fromISO(date.toISOString()).setLocale("es").toFormat("dd 'de' LLLL 'del' yyyy")}
-        </span>
-      <h1>Comic favoritos</h1>
-      <div>{user.comicsFav.map((comic) => {
-        return (
-            <div>{comic}</div>
-        );
-        })}</div>
-      <div>{user.comicsFav.map}</div>
-      <h1>Personajes favoritos</h1>
-      <div>{user.personajesFav.map((heroe) => {
-        return (
-            <div>{heroe}</div>
-        );
-        })}</div>
-    </div>
-  )
+  );
 }
--- marvelapp/src/pages/admin/SingleUser/SingleUser.js
+++ marvelapp/src/pages/admin/SingleUser/SingleUser.js
@@ -4,7 +4,6 @@ import { UserInfo } from "../../../components/Admin/"
 export function SingleUser() {
   return (
     <div>
-        <h1>Aqui va la info del usuario</h1>
         <UserInfo />
     </div>
   )
